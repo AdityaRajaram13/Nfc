3d4d7f8ca60ff17cf0221df5aa7c985e3c6c76ce
AdityaRajaram <aditya.rajaram13@gmail.com>
2023-07-14 17:17:41 +0530

    changes

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -58,43 +58,103 @@
 
 
 
-import React from 'react';
+
+
+// import React from 'react';
+// import '@fortawesome/fontawesome-free/css/all.min.css';
+
+// const Sidebar = () => {
+//   return (
+//     <div>
+//       <div className="flex w-16 flex-col items-center">
+//         <div className="space-y-48  bg-black">
+//           <ul>
+//             <li className="p-5">
+//               <i className="fas fa-columns h-6 w-6 cursor-pointer text-gray-500 transition-all hover:text-blue-600"></i>
+//             </li>
+//             <li className="p-5">
+//               <i className="fas fa-users h-6 w-6 cursor-pointer text-gray-500 transition-all hover:text-blue-600"></i>
+//             </li>
+//             <li className="p-5">
+//               <i className="fas fa-cog h-6 w-6 cursor-pointer text-gray-500 transition-all hover:text-blue-600"></i>
+//             </li>
+//             <li className="p-5">
+//               <i className="fas fa-envelope h-6 w-6 cursor-pointer text-gray-500 transition-all hover:text-blue-600"></i>
+//             </li>
+//             <li className="p-5">
+//               <i className="fas fa-file-alt h-6 w-6 cursor-pointer text-gray-500 transition-all hover:text-blue-600"></i>
+//             </li>
+//             <li className="p-5">
+//               <i className="fas fa-bell h-6 w-6 cursor-pointer text-gray-500 transition-all hover:text-blue-600"></i>
+//             </li>
+//           </ul>
+//         </div>
+//         {/* Rest of the component */}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Sidebar;
+
+
+
+
+import React,{useState}  from 'react';
+import Dashboard from '../Pages/Dashboard';
+import { BsGridFill } from 'react-icons/bs';
+import { BiUser } from 'react-icons/bi';
+import { AiOutlineEye } from 'react-icons/ai';
+import { TiChartBar } from 'react-icons/ti';
+import { FaHistory } from 'react-icons/fa';
 
 const Sidebar = () => {
+  const [showDashboard, setShowDashboard] = useState(false);
+
+  const handleGridFillClick = () => {
+    setShowDashboard(true);
+  };
+const Sidebar = () => {
   return (
-    <aside className="h-full w-16 flex flex-col space-y-10 items-center justify-center relative bg-gray-800 text-white">
-      {/* Profile */}
-      <div className="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white hover:duration-300 hover:ease-linear focus:bg-white">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-          <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
-        </svg>
+    <aside className="fixed top-0 left-0 h-screen w-16 flex flex-col space-y-10 items-center justify-center text-white" style={{ backgroundColor: "#111536" }}>
+      <div className="flex w-16 flex-col items-center">
+        <div className="space-y-48">
+          <ul>
+            <li className="p-5" onClick={handleGridFillClick}>
+              <BsGridFill
+                className="h-6 w-6 cursor-pointer text-gray-500 transition-all hover:text-blue-600"
+              />
+            </li>
+            <li className="p-5">
+              <BiUser
+                className="h-6 w-6 cursor-pointer text-gray-500 transition-all hover:text-blue-600"
+              />
+            </li>
+            <li className="p-5">
+              <AiOutlineEye
+                className="h-6 w-6 cursor-pointer text-gray-500 transition-all hover:text-blue-600"
+              />
+            </li>
+            <li className="p-5">
+              <TiChartBar
+                className="h-6 w-6 cursor-pointer text-gray-500 transition-all hover:text-blue-600"
+              />
+            </li>
+            <li className="p-5">
+              <FaHistory
+                className="h-6 w-6 cursor-pointer text-gray-500 transition-all hover:text-blue-600"
+              />
+            </li>
+            <li className="p-5">
+              <i className="fas fa-bell h-6 w-6 cursor-pointer text-gray-500 transition-all hover:text-blue-600"></i>
+            </li>
+          </ul>
+        </div>
+        {/* Rest of the component */}
       </div>
-
-      {/* Courses */}
-      <div className="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white hover:duration-300 hover:ease-linear focus:bg-white">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
-        </svg>
-      </div>
-
-      {/* Theme */}
-      <div className="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white hover:duration-300 hover:ease-linear focus:bg-white">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
-        </svg>
-      </div>
-
-      {/* Configuration */}
-      <div className="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white hover:duration-300 hover:ease-linear focus:bg-white">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-        </svg>
-      </div>
+      {showDashboard && <Dashboard />}
     </aside>
   );
 };
- export default Sidebar;
-
-
-
+}
+export default Sidebar;
