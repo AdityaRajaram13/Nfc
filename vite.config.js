b7b43189ec9d68da1d1a374b578cfd3adeabb5af
AdityaRajaram <aditya.rajaram13@gmail.com>
2023-10-06 17:39:06 +0530

    PWA CHANGES FINAL

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,21 @@
 // })
 
 
+// import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/',
+//   build: {
+//     outDir: 'dist', // Change to your preferred output directory
+//   },
+//   server: {
+//     open: true,
+//   },
+//   // Add other configuration options as needed
+// });
+
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
@@ -16,11 +31,17 @@ export default defineConfig({
   base: '/',
   build: {
     outDir: 'dist', // Change to your preferred output directory
+    rollupOptions: {
+      output: {
+        assetFileNames: '[name].[ext]', // Copy files with their original names and extensions
+      },
+    },
   },
   server: {
     open: true,
   },
   // Add other configuration options as needed
 });
+
 
 
