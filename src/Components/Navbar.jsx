f668d1ea792bf323236cb44c19d904bf5fda50f4
AdityaRajaram <aditya.rajaram13@gmail.com>
2024-02-10 00:12:01 +0530

    AddLogo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -101,7 +101,7 @@
 //             )}
 //           </button>
 //         </li>
-       
+
 //       </ul>
 //       <div className="flex flex-shrink-0 items-center space-x-4 text-white">
 //       <div className="relative">
@@ -156,7 +156,7 @@
 //       {
 //       isOpen &&(
 //       <div className="fixed inset-0 w-[calc(100%-4.5rem)] translate-x-[-20%] bg-blue-200 border-r shadow-xl transition duration-300 lg:border-r-0 lg:w-auto lg:static lg:shadow-none lg:translate-x-0">        <div className="text-xl md:hidden flex items-center justify-center right-0 py-4 font-bold">
-         
+
 //           <img
 //             src={profilePng} // Use the imported profilePng variable here
 //             alt="Profile"
@@ -181,7 +181,7 @@
 //                         My Profile
 //                       </li>
 //                       <li className="md:py-2" 
-                      
+
 //                       onClick={handleLogout}>
 //                         Logout
 //                       </li>
@@ -246,15 +246,15 @@
 //           </li>
 //         </ul>
 
-          
-//         </div>
-     
 
-   
+//         </div>
+
+
+
 //       )}
-      
+
 //     </header>
-    
+
 
 //   );
 // };
@@ -262,7 +262,7 @@
 // export default Navbar;
 
 import React, { useState, useEffect, useRef, useContext } from 'react';
-import { Link,useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { logoutUser } from '../utils/auth';
 import { AuthContext } from '../utils/authContext'; // Import the AuthContext
 import { CgProfile } from 'react-icons/cg';
@@ -270,51 +270,66 @@ import Hamburger from 'hamburger-react';
 
 
 const Navbar = () => {
-  const [showDropdown, setShowDropdown] = useState(false);
-  const [showCurrencyDropdown, setShowCurrencyDropdown] = useState(false);
-  const [showProductDropdown, setShowProductDropdown] = useState(false);
-  const dropdownRef = useRef(null);
+  // const [showCurrencyDropdown, setShowCurrencyDropdown] = useState(false);
+  // const [showProductDropdown, setShowProductDropdown] = useState(false);
   const navigate = useNavigate();
+  const dropdownRef = useRef(null);
   const { userLoggedIn, setUserLoggedIn } = useContext(AuthContext); // Use the userLoggedIn state from context
+
+  const [showDropdown, setShowDropdown] = useState(false);
   const [isOpen, setOpen] = useState(false);
+  const [toggleMobileDropdowns, setToggleMobileDropdowns] = useState({
+    product: false,
+    profile: false,
+    currency: false,
+  })
+  const [toggleDropdowns, setToggleDropdowns] = useState({
+    product: false,
+    profile: false,
+    currency: false,
+  })
 
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        setShowDropdown(false);
-        setShowCurrencyDropdown(false);
-        setShowProductDropdown(false);
-      }
-    };
 
-    document.addEventListener('mousedown', handleClickOutside);
+  // useEffect(() => {
+  //   const handleClickOutside = (event) => {
+  //     console.log("here")
+  //     if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+  //       setShowDropdown(!showDropdown);
+  //       setShowCurrencyDropdown(!showCurrencyDropdown);
+  //       setShowProductDropdown(!showProductDropdown);
 
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
+  //     }
+  //   };
 
-  const toggleDropdown = () => {
-    setShowDropdown(!showDropdown);
-  };
+  //   document.addEventListener('mousedown', handleClickOutside);
+
+  //   return () => {
+  //     document.removeEventListener('mousedown', handleClickOutside);
+  //   };
+  // }, []);
+
+  // const toggleDropdown = () => {
+  //   setShowDropdown(!showDropdown);
+  //   console.log("click");
+  // };
 
   const handleDashboardClick = () => {
     navigate('/dashboard');
   };
 
-  const handleProductDropdownHover = (isHovering) => {
-    setShowProductDropdown(isHovering || showProductDropdown);
-  };
+  // const handleProductDropdownHover = (isHovering) => {
+  //   setShowProductDropdown(isHovering || showProductDropdown);
+  // };
 
-  const handleCurrencyDropdownHover = (isHovering) => {
-    setShowCurrencyDropdown(isHovering || showCurrencyDropdown);
-  };
+  // const handleCurrencyDropdownHover = (isHovering) => {
+  //   setShowCurrencyDropdown(isHovering || showCurrencyDropdown);
+  // };
 
-  const handleDropdownOptionClick = () => {
-    setShowDropdown(false);
-    setShowCurrencyDropdown(false);
-    setShowProductDropdown(false);
-  };
+  // const handleDropdownOptionClick = () => {
+  //   setShowDropdown(false);
+  //   setShowCurrencyDropdown(false);
+  //   setShowProductDropdown(false);
+  // };
 
   const handleLogout = async () => {
     console.log('Logging out...');
@@ -339,44 +354,49 @@ const Navbar = () => {
   };
 
   return (
-    <header className="h-16 w-full  items-center relative z-50 justify-end px-5 space-x-10 border-b-2 border-blue-600" style={{ backgroundColor: "#111536" }}>
+    <header className="h-16 w-full  items-center relative z-50 justify-end px-5  border-b-2 border-blue-600" style={{ backgroundColor: "#111536" }}>
       <div className="h-16 hidden md:flex w-full  items-center relative z-50 justify-end px-5 space-x-10 border-b-2" style={{ backgroundColor: "#111536" }}>
-      <ul className="flex space-x-4 text-white">
-      <span className="text-white text-xl mr-[1150px] font-bold"> Logo </span>
-        <li className="font-medium"> <Link to="/">Home</Link></li>
-        <li className="relative">
-        <button
-            className="font-medium"
-            onClick={() => handleProductDropdownHover(true)}
-            onMouseLeave={() => handleProductDropdownHover(false)}
-          >
-            Products
-            {showProductDropdown && (
-              <div className="absolute top-full -right-14 w-40 bg-blue-950 shadow rounded-md mt-5" ref={dropdownRef}>
-                <ul className="py-2 px-4 space-y-2">
-                  <li className="text-white font-Inter cursor-pointer hover:bg-violet-900 py-1 px-2 rounded-md" onClick={handleDropdownOptionClick}>
-                  <Link to="/products" className="block">
-                    Cards
-                  </Link>
-                  </li>
-                </ul>
-              </div>
-            )}
-          </button>
-        </li>   
-      </ul>
-      <div className="flex flex-shrink-0 items-center space-x-4 text-white">
+        <div className='flex w-full justify-start  text-2xl text-white'>
+          <p>Logo</p>
+        </div>
+        <ul className="flex space-x-4 text-white">
+          <li className="font-medium"> <Link to="/">Home</Link></li>
+          <li className="relative">
+            <button
+              className="font-medium"
+              // onClick={() => handleProductDropdownHover(true)}
+              onClick={() => setToggleDropdowns({ ...toggleDropdowns, product: !toggleDropdowns.product })}
+            // onClick={}
+            // onMouseLeave={() => handleProductDropdownHover(false)}
+            >
+              Products
+              {toggleDropdowns.product && (
+                <div className="absolute top-full -right-14 w-40 bg-blue-950 shadow rounded-md mt-5" ref={dropdownRef}>
+                  <ul className="py-2 px-4 space-y-2">
+                    {/* <li className="text-white font-Inter cursor-pointer hover:bg-violet-900 py-1 px-2 rounded-md" onClick={handleDropdownOptionClick}> */}
+                    <li className="text-white font-Inter cursor-pointer hover:bg-violet-900 py-1 px-2 rounded-md">
+                      <Link to="/products" className="block">
+                        Cards
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              )}
+            </button>
+          </li>
+        </ul>
+        <div className="flex flex-shrink-0 items-center space-x-4 text-white">
 
-      <div className="relative">
-          <CgProfile
-            className="h-10 w-10 rounded-full cursor-pointer border-2 border-blue-400"
-            onClick={() => setShowDropdown(true)}
-          />
-            {showDropdown && (
+          <div className="relative">
+            <CgProfile
+              className="h-10 w-10 rounded-full cursor-pointer border-2 border-blue-400"
+              onClick={() => setToggleDropdowns({ ...toggleDropdowns, profile: !toggleDropdowns.profile })}
+            />
+            {toggleDropdowns.profile && (
               <div className="absolute top-full right-0 w-40 bg-blue-950 shadow rounded-md mt-3" ref={dropdownRef}>
-                
+
                 <ul className="py-2 px-4 space-y-2">
-                {userLoggedIn ? (
+                  {userLoggedIn ? (
                     <>
                       <li className="text-white font-Inter cursor-pointer hover:bg-violet-900 py-1 px-2 rounded-md" onClick={handleDashboardClick}>
                         My Profile
@@ -402,119 +422,128 @@ const Navbar = () => {
                 </ul>
               </div>
             )}
+          </div>
         </div>
       </div>
-      </div>
+
       <div className="md:hidden flex justify-end right-0 py-2" >
-      <Hamburger
-        toggled={isOpen}
-        toggle={toggleMenu}
-        color="#4FD1C5" 
-        className="m-4 z-[70]"      
-      />
-      </div>
-      {
-      isOpen &&(
-      <div className="fixed inset-0 w-[calc(100%-4.5rem)] translate-x-[-35%] bg-pink-300 border-r shadow-xl transition duration-300 lg:border-r-0 lg:w-auto lg:static lg:shadow-none lg:translate-x-0">                
-       
-       <ul className="flex-col ml-2  mt-20 text-center flex-wrap">
-       <li className="relative mt-2  md:py-2 text-xl">
-
-
-       <div
-           onClick={() => setShowDropdown(true)}
-           className="px-10 py-2 font-Poppins md:px-9 md:py-4  hover:bg-pink-600 text-white font-bold"
-           >
-             Profile
-             {showDropdown && (
-              <div
-                ref={dropdownRef}
-                className="absolute left-0 right-0 mt-3 bg-blue-400"
-              >
-                <ul className="text-center">
-                  {userLoggedIn ? (
-                    <>
-                      <li
-                        onClick={() => {handleDashboardClick;
-                          setOpen(false);
-                        }}
-                      >
-                        My Profile
-                      </li>
-                      <li 
-                      onClick={handleLogout}>
-                        Logout
-                      </li>
-                    </>
-                  ) : (
-                    <>
-                      <Link
-                        to="/signin"
-                        onClick={() => {setShowDropdown(false);
-                          setOpen(false);
-                        }}
-                        className="text-white font-Poppins hover:text-blue-300"
-                      >
-                        <li >Log In</li>
-                      </Link>
-                      <Link
-                        to="/signup"
-                        onClick={() =>{ setShowDropdown(false);
-                          setOpen(false);
-
-                        }}
-                        className="text-white font-Poppins hover:text-blue-300"
-                      >
-                        <li className="md:py-2">Create Profile</li>
-                      </Link>
-                    </>
-                  )}      
-                 <li/>         
-                </ul>
-            </div>   
-            )}
-        </div>        
-         </li>
-
-          <li className=" py-1  text-xl">
-            <Link to="/" className="px-10 py-2 font-Poppins md:px-9 md:py-4  hover:bg-pink-600 text-white font-bold">
-              Home
-            </Link>
-          </li>
-
-          <li className="relative mt-2  md:py-2 text-xl">
-            <button
-              onClick={() => handleProductDropdownHover(true)}
-              onMouseLeave={() => handleProductDropdownHover(false)}
-              className="px-10 py-2 font-Poppins md:px-9 md:py-4  hover:bg-pink-600 text-white font-bold"
-            >
-              Products
-              {showProductDropdown && (
-                <div
-                  ref={dropdownRef}
-                  className="absolute left-0 right-0 mt-3 bg-blue-400 "
-                >
-                  <ul className="  text-center">
-                    <li  onClick={handleDropdownOptionClick}>
-                      <Link to="/products" className="text-white font-Poppins hover:text-blue=-300">
-                        Cards
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-              )}
-            </button>
-            
-          </li>
-        </ul>
+        <div className='flex w-full justify-start item-center text-2xl text-white'>
+          <p>Logo</p>
         </div>
-     
+        <Hamburger
+          toggled={isOpen}
+          toggle={toggleMenu}
+          color="#4FD1C5"
+          className="m-4 z-[70]"
+        />
+      </div>
 
-   
-      )}
-      
+      {
+        isOpen && (
+          <div className="fixed md:hidden  inset-0 w-[60%]  bg-pink-300 border-r shadow-xl transition duration-300">
+
+            <ul className="flex-col ml-2  mt-20 text-center flex-wrap">
+              <li className="relative mt-2  text-xl">
+                <div
+                  onClick={() => setToggleMobileDropdowns({ ...toggleMobileDropdowns, profile: !toggleMobileDropdowns.profile })}
+                  // onClick={}
+                  className="px-10 py-2 font-Poppins md:px-9 md:py-4  hover:bg-pink-600 text-white font-bold"
+                >
+                  Profile
+                  {toggleMobileDropdowns.profile && (
+                    <div
+                      ref={dropdownRef}
+                      className="mt-3 bg-blue-400"
+                    >
+                      <ul className="text-center">
+                        {userLoggedIn ? (
+                          <>
+                            <li
+                              onClick={() => {
+                                handleDashboardClick;
+                                setOpen(false);
+                              }}
+                            >
+                              My Profile
+                            </li>
+                            <li
+                              onClick={handleLogout}>
+                              Logout
+                            </li>
+                          </>
+                        ) : (
+                          <>
+                            <Link
+                              to="/signin"
+                              onClick={() => {
+                                setShowDropdown(false);
+                                setOpen(false);
+                              }}
+                              className="text-white font-Poppins hover:text-blue-300"
+                            >
+                              <li >Log In</li>
+                            </Link>
+                            <Link
+                              to="/signup"
+                              onClick={() => {
+                                setShowDropdown(false);
+                                setOpen(false);
+
+                              }}
+                              className="text-white font-Poppins hover:text-blue-300"
+                            >
+                              <li className="md:py-2">Create Profile</li>
+                            </Link>
+                          </>
+                        )}
+                        <li />
+                      </ul>
+                    </div>
+                  )}
+                </div>
+              </li>
+
+              <li className=" py-1  text-xl">
+                <Link to="/" className="px-10 py-2 font-Poppins md:px-9 md:py-4  hover:bg-pink-600 text-white font-bold">
+                  Home
+                </Link>
+              </li>
+
+              <li className="relative mt-2  md:py-2 text-xl">
+                <button
+                  // onClick={() => handleProductDropdownHover(true)}
+                  onClick={() => setToggleMobileDropdowns({ ...toggleMobileDropdowns, product: !toggleMobileDropdowns.product })}
+                  onMouseLeave={() => handleProductDropdownHover(false)}
+                  className="px-10 py-2 font-Poppins md:px-9 md:py-4  hover:bg-pink-600 text-white font-bold"
+                >
+                  Products
+                  {toggleMobileDropdowns.product && (
+                    <div
+                      ref={dropdownRef}
+                      className="absolute left-0 right-0 mt-3 bg-blue-400 "
+                    >
+                      <ul className="  text-center">
+                        {/* <li onClick={handleDropdownOptionClick}> */}
+                        <li>
+                          <Link to="/products" className="text-white font-Poppins hover:text-blue=-300">
+                            Cards
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
+                  )}
+                </button>
+
+              </li>
+            </ul>
+          </div>
+
+
+
+        )}
+
     </header>
-    
+
 
   );
 };
